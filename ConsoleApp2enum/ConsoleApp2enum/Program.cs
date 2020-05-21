59053dc92af9b35dc1802d4c57134590ea037325
wxf1116 <3720441@qq.com>
2020-05-21 17:12:08 +0800

    枚举

---
+++ ConsoleApp2enum/ConsoleApp2enum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp2enum
+{
+    class Program
+    {
+        public enum TimeOfDay
+        {
+            Morning = 0,
+            Afternoon = 1,
+            Evening = 2
+        }
+        static int Main(string[] args)
+        {
+            WriteGreeting(TimeOfDay.Morning);
+            return 0;
+        }
+        static void WriteGreeting(TimeOfDay timeOfDay)
+        {
+            switch (timeOfDay)
+            {
+                case TimeOfDay.Morning:
+                    Console.WriteLine("Good Morning!");
+                    break;
+                case TimeOfDay.Afternoon:
+                    Console.WriteLine("Good Afternoon!");
+                    break;
+                case TimeOfDay.Evening:
+                    Console.WriteLine("Good Evening!");
+                    break;
+                default:
+                    Console.WriteLine("Hello");
+                    break;
+            }
+        }
+    }
+}
