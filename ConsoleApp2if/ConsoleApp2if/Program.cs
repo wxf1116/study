6df22b04da26ffb1618f5c90bedd8fa9bcb4b11a
wxf1116 <3720441@qq.com>
2020-05-21 16:18:01 +0800

    if语句

---
+++ ConsoleApp2if/ConsoleApp2if/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp2if
+{
+    /*
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isZero;
+            int i = 1;
+            if(i == 0)
+                {
+                isZero = true;
+                Console.WriteLine("i is Zero");
+                }
+            else
+            {
+                isZero = false;
+                Console.WriteLine("i is No-zero");
+            }
+        }
+    }
+    */
+    class MainEntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a string");
+            string input;
+            input = Console.ReadLine();
+            if(input == "")
+            {
+                Console.WriteLine("You typed in an empty string.");
+            }
+            else if(input.Length < 5)
+            {
+                Console.WriteLine("The string had less than 5 characters");
+            }
+            else if(input.Length < 10)
+            {
+                Console.WriteLine("The string had at least 5 but less than 10 Characters.");
+            }
+            Console.WriteLine("The string was " + input);
+        }
+    }
+}
