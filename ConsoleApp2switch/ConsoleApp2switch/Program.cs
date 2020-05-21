41bb1008ab91a780ccc8237c025d8c2251f6b3e3
wxf1116 <3720441@qq.com>
2020-05-21 16:34:15 +0800

    switch语句

---
+++ ConsoleApp2switch/ConsoleApp2switch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integerA = 1;
+            switch (integerA)
+            {
+                case 1:
+                    Console.WriteLine("integerA = 1");
+                    break;
+                case 2:
+                    Console.WriteLine("integerA = 2");
+                    break;
+                case 3:
+                    Console.WriteLine("integerA = 3");
+                    break;
+                default:
+                    Console.WriteLine("integerA is not 1,2or3");
+                    break;
+            }
+        }
+    }
+}
