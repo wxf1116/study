2bfe0359b49001c37c025767a3cd71335907a11f
wxf1116 <3720441@qq.com>
2020-05-21 14:18:10 +0800

    变量

---
+++ ConsoleApp2var/ConsoleApp2var/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Bugs Bunny";
+            var age = 25;
+            var isRabbit = true;
+
+            Type nameType = name.GetType();
+            Type ageType = age.GetType();
+            Type isRabbitType = isRabbit.GetType();
+
+            Console.WriteLine("name is type" + nameType.ToString());
+            Console.WriteLine("age is type" + ageType.ToString());
+            Console.WriteLine("isRabbit is type" + isRabbitType.ToString());
+        }
+    }
+}
