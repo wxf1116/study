cad6a3f90d05c312539059d449051ce3e47ad976
wxf1116 <3720441@qq.com>
2020-05-21 16:53:43 +0800

    for语句

---
+++ ConsoleApp2for/ConsoleApp2for/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for (int i = 0; i<100; i+=10)
+            {
+                for (int j = i; j< i+10; j++)
+                {
+                    Console.WriteLine(" " + j);
+                }
+                Console.WriteLine();
+            }
+            */
+            int[] arrayOfInts = { 1, 2, 3 };
+            foreach ( int temp in arrayOfInts)
+            {
+                Console.WriteLine(temp);
+            }
+        }
+    }
+}
