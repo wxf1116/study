087073eab6e31716dd92b8a0263387134c163c1f
wxf1116 <3720441@qq.com>
2020-05-22 09:49:10 +0800

    Console.WriteLine

---
+++ ConsoleApp2console/ConsoleApp2console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int i = 10;
+            int j = 20;
+            Console.WriteLine("{0}push{1}equals{2}", i, j, i + j);
+            
+            int i = 940;
+            int j = 73;
+            Console.WriteLine("{0,1:C2}\n+{1,4:C4}\n----\n{2,4}", i, j, i + j);
+            */
+            double d = 0.234;
+            Console.WriteLine("{0:#.0000}", d);
+        }
+    }
+}
