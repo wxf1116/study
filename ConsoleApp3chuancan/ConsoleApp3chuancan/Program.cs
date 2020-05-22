cee6d78954a0c62fa322e5b6e932dd493a9b6eac
wxf1116 <3720441@qq.com>
2020-05-22 11:44:49 +0800

    值类型与引用类型

---
+++ ConsoleApp3chuancan/ConsoleApp3chuancan/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Wrox
+{
+    class ParameterTest
+    {
+        static void SomeFunction(int[] ints, int i)
+        {
+            ints[0] = 100;
+            i = 100;
+        }
+        public static int Main()
+        {
+            int i = 0;
+            int[] ints = { 0, 1, 2, 3, 6, 8 };
+            Console.WriteLine("i is" + i);
+            Console.WriteLine("ints is" + ints[0]);
+            SomeFunction(ints, i);
+            Console.WriteLine("i is" + i);
+            Console.WriteLine("ints is" + ints[0]);
+            return 0;
+        }
+    }
+}
