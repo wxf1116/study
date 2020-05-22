07d7e5dcc3e6abc1cba20a3a1988a76f631d3d38
wxf1116 <3720441@qq.com>
2020-05-22 09:11:16 +0800

    命名空间

---
+++ ConsoleApp2using2/ConsoleApp2using2/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wrox.ProCSharp.Basics
+{
+    class NamespaceExample
+    {
+        public int GetNamespace()
+        {
+            Console.WriteLine("is GetNamespace");
+            return 0;
+        }
+    }
+}
---
+++ ConsoleApp2using2/ConsoleApp2using2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Introduction = Wrox.ProCSharp.Basics;
+
+class Test
+{
+    public static int Main()
+    {
+        Introduction::NamespaceExample NSEx =
+            new Introduction::NamespaceExample();
+        Console.WriteLine(NSEx.GetNamespace());
+        return 0;
+    }
+}
