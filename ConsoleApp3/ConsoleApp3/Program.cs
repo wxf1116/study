bd89ea91e627ac579424b9820cbe6726b62ee0d3
wxf1116 <3720441@qq.com>
2020-05-22 11:16:27 +0800

    声明方法

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Rectangle
+    {
+        public int Height = 88;
+        public int Width = 88;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            Console.WriteLine(Program.IsSquare(rect));
+            Console.WriteLine(IsPositive(rect.Height));
+        }
+        public static bool IsSquare (Rectangle rect)
+        {
+            return (rect.Height == rect.Width);
+        }
+        public static bool IsPositive (int Value)
+        {
+            if (Value < 0)
+                return false;
+            return true;
+        }
+    }
+}
