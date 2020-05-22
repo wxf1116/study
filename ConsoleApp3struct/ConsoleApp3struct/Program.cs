cd28836f23483577b48a35d76514a309f21be358
wxf1116 <3720441@qq.com>
2020-05-22 16:29:42 +0800

    结构

---
+++ ConsoleApp3struct/ConsoleApp3struct/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3struct
+{
+    struct Dimen
+    {
+        public double Length;
+        public double Width;
+
+        public Dimen(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public double Diagona1
+        {
+            get
+            {
+                return Math.Sqrt(Length * Length + Width * Width);
+            }
+        }
+        static void Main()
+        {
+            Dimen dimen = new Dimen(2, 2);
+            Console.WriteLine(dimen.Diagona1);
+        }
+    }
+}
