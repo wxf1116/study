bf552d2c63451c18fc1a5dea07b5da5304e8437c
wxf1116 <3720441@qq.com>
2020-05-22 11:03:38 +0800

    类

---
+++ ConsoleApp3class/ConsoleApp3class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3class
+{
+    class Program //声明一个类
+    {
+        static void Main(string[] args)
+        {
+            PhoneCustomer myCustomer = new PhoneCustomer();
+            PhoneCustomer1 myCustomer2 = new PhoneCustomer1();
+            Console.WriteLine(myCustomer.DayOfSendingBill);
+            myCustomer.CustomerID = 999;
+            Console.WriteLine(myCustomer.CustomerID);
+            myCustomer2.FirstName = "Simon";
+        }
+    }
+    class PhoneCustomer //声明一个类
+    {
+        public readonly  string DayOfSendingBill = "Monday";
+        public int CustomerID;
+        public string FirstName;
+        public string LastName;
+    }
+    struct PhoneCustomer1 //声明一个结构
+    {
+        public const string DayOfSendingBill = "Monday";
+        public int CustomerID;
+        public string FirstName;
+        public string LastName;
+    }
+}
