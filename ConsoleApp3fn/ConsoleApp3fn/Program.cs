63aa485c1027575110596425b8cd4ebf3a755871
wxf1116 <3720441@qq.com>
2020-05-22 11:31:21 +0800

    调用方法

---
+++ ConsoleApp3fn/ConsoleApp3fn/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Wrox
+{
+    class MainEntryPoint
+    {
+        static void Main()
+        {
+            Console.WriteLine("pi is" + MathTest.GetPi());
+            int x = MathTest.GetSquareOf(5);
+            Console.WriteLine("Square of 5 is" + x);
+            MathTest math = new MathTest();
+            math.value = 30;
+            Console.WriteLine("Value..." + math.value);
+            Console.WriteLine("Square of 30 is" + math.GetSquare());
+        }
+    }
+    class MathTest
+    {
+        public int value;
+        public int GetSquare()
+        {
+            return value * value;
+        }
+        public static int GetSquareOf(int x)
+        {
+            return x * x;
+        }
+        public static double GetPi()
+        {
+            return 3.1415926;
+        }
+    }
+}
