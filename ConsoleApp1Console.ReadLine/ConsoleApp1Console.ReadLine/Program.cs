349f5eccada7b6339b2a95bac37c6c2789fe3824
wxf1116 <3720441@qq.com>
2020-05-23 19:20:40 +0800

    打招呼

---
+++ ConsoleApp1Console.ReadLine/ConsoleApp1Console.ReadLine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1Console.ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("请输入姓名:");
+            name = Console.ReadLine();
+            Console.WriteLine("你好！{0}!", name);
+        }
+    }
+}
