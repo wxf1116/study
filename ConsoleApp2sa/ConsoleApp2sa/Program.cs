fd8f64268ffa90fa94de71578528f8e36332b40a
wxf1116 <3720441@qq.com>
2020-05-23 22:24:09 +0800

    求平均分和总分

---
+++ ConsoleApp2sa/ConsoleApp2sa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2sa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = new string[5];
+            int[] score = new int[5];
+            for (int i = 0; i < name.Length; i++)
+            {
+
+                Console.Write("请输入第"+(i+1)+"位同学姓名:");
+                name[i] = Console.ReadLine();
+                Console.Write("请输入第" + (i + 1) + "位同学分数:");
+                score[i] =int.Parse( Console.ReadLine());
+
+            }
+            int sum = 0, avg;
+            for (int i = 0; i < score.Length; i++)
+            {
+                sum += score[i];
+            }
+            avg = sum / score.Length;
+
+            Console.WriteLine("总分：" + sum + "平均分：" + avg);
+        }
+    }
+}
