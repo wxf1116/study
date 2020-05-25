be3a67d7542b1013058e3661dc81d0de9d4a1433
wxf1116 <3720441@qq.com>
2020-05-25 10:43:18 +0800

    只读字段

---
+++ readonly/readonly/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abcreadonly
+{
+    public class Document
+    {
+        public readonly DateTime CreationDate;
+        public Document()
+        {
+            CreationDate = new DateTime(2020, 11, 16);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document doc = new Document();
+            Console.WriteLine(doc.CreationDate);
+        }
+    }
+}
