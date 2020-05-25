acd547d1a894ece8d9d60eda224f66eb08d7d511
wxf1116 <3720441@qq.com>
2020-05-25 15:45:24 +0800

    锯齿数组

---
+++ enumColors/enumColors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] [] jagged = new int[3][];
+            jagged[0] = new int[2] { 1, 2 };
+            jagged[1] = new int[6] { 3, 4, 5, 6, 7, 8 };
+            jagged[2] = new int[3] { 9, 10, 11 };
+            for (int i = 0; i < jagged.Length; i++)
+            {
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    Console.WriteLine("row:{0},element:{1},value:{2}", i, j, jagged[i][j]);
+                }
+            }
+        }
+    }
+}
