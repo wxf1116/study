c78d6d0a13ca80249de6af0bd6e1a58ed2ba5743
wxf1116 <3720441@qq.com>
2020-05-25 09:51:41 +0800

    今天颜色

---
+++ ConsoleApp2backcolor/ConsoleApp2backcolor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ConsoleApp2backcolor
+{
+    public class UserPreferences
+    {
+        public static readonly Color BackColor;
+
+        static UserPreferences()
+        {
+            DateTime now = DateTime.Now;
+            if (now.DayOfWeek == DayOfWeek.Saturday || now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                BackColor = Color.Green;
+            }else
+            {
+                BackColor = Color.Red;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Color is:{0}", UserPreferences.BackColor);
+        }
+    }
+}
