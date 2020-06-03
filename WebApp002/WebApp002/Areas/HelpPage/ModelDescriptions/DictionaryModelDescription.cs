2ec409f6507d31ad4255e1bef27f2bd598111151
wxf1116 <3720441@qq.com>
2020-06-03 14:30:34 +0800

    MVC路由与动作方法

---
+++ WebApp002/WebApp002/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApp002.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApp002/WebApp002/Controllers/ResultController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using WebApp002.Models;
+
+namespace WebApp002.Controllers
+{
+    public class ResultController : Controller
+    {
+        // GET: Result
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult contentDemo()
+        {
+            return Content("Hello, World", "text/plain");
+        }
+
+        public ActionResult JavaScriptDemo()
+        {
+            return JavaScript("<script>function foo (){alert('foo');}</script>");
+        }
+
+        public ActionResult JsonDemo()
+        {
+            var m = new Menu1
+            {
+                Id = 3,
+                Text = "sdjiwjijidfjsaid",
+                price = 12.23,
+                Category = "Main"
+            };
+
+            return Json(m, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult RedirectDemo()
+        {
+            return Redirect("https://www.baidu.com");
+        }
+
+        public ActionResult RedirectRouteDemo()
+        {
+            return RedirectToRoute(new { controller = "Result" , action = "JsonDemo"});
+        }
+
+        public ActionResult FileDemo()
+        {
+            return File("~/Content/bootstrap.css", "css");
+        }
+    }
+}
