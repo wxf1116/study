193e6c592a537cb1c742a379bb7dde4c96481f54
wxf1116 <3720441@qq.com>
2020-06-03 21:35:52 +0800

    学生表

---
+++ StudentTable/StudentTable/Student/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace StudentTable.Student
+{
+    public class Student
+    {
+        string sqlselect = "server = localhost; database = test; user = root; password = 123456; charset = utf8";
+        public void select()
+        {
+            using(MySqlConnection conn = new MySqlConnection(sqlselect))
+            {
+                try
+                {
+                    conn.Open();
+                    string sqlselect = "select * from student";
+                    MySqlCommand cmd = new MySqlCommand(sqlselect, conn);
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
